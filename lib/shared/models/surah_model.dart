b936cb9269c73565216fea571d64c0fb104b2a81
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2025-08-07 22:41:38 +0700

    feat : add surah model on shared

---
+++ lib/shared/models/surah_model.dart
@@ -0,0 +1,34 @@
+class SurahModel {
+  final int nomor;
+  final String nama;
+  final String namaLatin;
+  final int jumlahAyat;
+  final String tempatTurun;
+  final String arti;
+  final String deskripsi;
+  final String audio;
+
+  SurahModel({
+    required this.nomor,
+    required this.nama,
+    required this.namaLatin,
+    required this.jumlahAyat,
+    required this.tempatTurun,
+    required this.arti,
+    required this.deskripsi,
+    required this.audio,
+  });
+
+  factory SurahModel.fromJson(Map<String, dynamic> json) {
+    return SurahModel(
+      nomor: json['nomor'],
+      nama: json['nama'],
+      namaLatin: json['nama_latin'],
+      jumlahAyat: json['jumlah_ayat'],
+      tempatTurun: json['tempat_turun'],
+      arti: json['arti'],
+      deskripsi: json['deskripsi'],
+      audio: json['audio'],
+    );
+  }
+}
