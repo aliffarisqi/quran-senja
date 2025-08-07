dd16d1ed98f686ffe1050972597a919552ca40c1
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2025-08-07 21:08:48 +0700

    update : beranda screen to add header and search box

--- lib/features/beranda/screen/beranda_screen.dart
+++ lib/features/beranda/screen/beranda_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../../../common/widget/appbar/appbar.dart';
+import '../../../common/widget/shape/app_spacing.dart';
+import '../../../common/widget/shape/search_container.dart';
 import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/sizes.dart';
 import '../../../utils/constants/text_strings.dart';
+import 'widget/delegate/search_bar_delegate.dart';
+import 'widget/header_beranda.dart';
 
 class BerandaScreen extends StatelessWidget {
   const BerandaScreen({super.key});
@@ -9,15 +15,43 @@ class BerandaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(),
+      appBar: _appBar(),
       backgroundColor: QSColors.white,
+      body: CustomScrollView(
+        slivers: [
+          // Header
+          _header(context),
+
+          // Sticky Search
+          SliverToBoxAdapter(child: QSAppSpacing.verticalSm),
+          _search(),
+        ],
+      ),
     );
   }
 
-  AppBar appBar() {
-    return AppBar(
-      title: Text(QSTexts.beranda),
+  _appBar() {
+    return QSAppBar(
+      showBackArrow: false,
       backgroundColor: QSColors.primaryLight,
+      isHide: true,
     );
   }
+
+  _search() {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: QSSearchBarDelegate(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: QSSizes.spacingLg),
+          child: QSSearchContainer(
+            text: QSTexts.searchPlaceHolder,
+            onTap: () {},
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(BuildContext context) => SliverToBoxAdapter(child: HeaderBeranda());
 }
