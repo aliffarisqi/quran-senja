eb54433d8057fab32140c65d39249bdd4ddcf924
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2025-08-07 21:08:10 +0700

    add : header of beranda page

---
+++ lib/features/beranda/screen/widget/header_beranda.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:quransenja/utils/extensions/context_extension.dart';
+
+import '../../../../common/widget/shape/app_spacing.dart';
+import '../../../../common/widget/space/rounded_container.dart';
+import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/text_strings.dart';
+
+/// A custom header widget for the home screen (Beranda).
+///
+/// This widget displays the application name and a greeting message inside a
+/// rounded container with a linear gradient background.
+class HeaderBeranda extends StatelessWidget {
+  const HeaderBeranda({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return QSRoundedContainer(
+      // Sets a linear gradient background for the container
+      gradientColor: QSColors.linerGradient,
+      // Sets the container's radius to 0 for a non-rounded, full-width look
+      radius: 0,
+      // Applies padding to the container's content
+      padding: const EdgeInsets.only(
+        left: QSSizes.spacingLg,
+        right: QSSizes.spacingLg,
+        top: QSSizes.spacingLg,
+      ),
+      child: Column(
+        // Aligns children to the start of the cross-axis (left)
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Displays the app name with a specific headline style and color
+          Text(
+            QSTexts.appName,
+            style: context.textTheme.headlineLarge?.copyWith(
+              color: QSColors.primaryMedium,
+            ),
+          ),
+          // Adds a small vertical space
+          QSAppSpacing.verticalSm,
+
+          // Displays the greeting sentence with a body style
+          Text(
+            QSTexts.greetingBeranda,
+            style: context.textTheme.bodyMedium,
+          ),
+          // Adds a small vertical space
+          QSAppSpacing.verticalSm,
+        ],
+      ),
+    );
+  }
+}
